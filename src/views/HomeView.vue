e272a01904db78853a621bcd7a94aff25d7d7762
ThanhTai <thanhtaiho188@gmail.com>
2022-07-02 16:17:58 +0700

    UsingProps_in_setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList :posts="posts"/>
+  </div>
+</template>
+<script>
+import { ref } from '@vue/reactivity'
+import PostList from '../components/PostList.vue';
+export default {
+    name: "HomeView",
+    components:{PostList},
+    setup() {
+        const posts = ref([
+            {
+                title: "welcome o the blog",
+                body: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptas optio sit cupiditate blanditiis dicta, sunt quisquam eaque quo, in eligendi repudiandae! Quia accusamus sunt nobis voluptatum porro at corrupti!Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptas optio sit cupiditate blanditiis dicta, sunt quisquam eaque quo, in eligendi repudiandae! Quia accusamus sunt nobis voluptatum porro at corrupti!Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptas optio sit cupiditate blanditiis dicta, sunt quisquam eaque quo, in eligendi repudiandae! Quia accusamus sunt nobis voluptatum porro at corrupti!Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptas optio sit cupiditate blanditiis dicta, sunt quisquam eaque quo, in eligendi repudiandae! Quia accusamus sunt nobis voluptatum porro at corrupti!Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptas optio sit cupiditate blanditiis dicta, sunt quisquam eaque quo, in eligendi repudiandae! Quia accusamus sunt nobis voluptatum porro at corrupti!Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptas optio sit cupiditate blanditiis dicta, sunt quisquam eaque quo, in eligendi repudiandae! Quia accusamus sunt nobis voluptatum porro at corrupti!",
+                id: 1
+            },
+            {
+                title: "top 5 CSS tip",
+                body: "loremipsum",
+                id: 2
+            }
+        ]);
+        return { posts };
+    },
+     
+}
+</script>
